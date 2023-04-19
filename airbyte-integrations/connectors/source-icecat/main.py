4a85d45f78cf3796453ab86a0198b908df609408
Sebastian Daum <daumse@mediamarktsaturn.com>
2023-04-19 12:16:09 +0200

    initialized icecat-source

---
+++ airbyte-integrations/connectors/source-icecat/main.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+import sys
+
+from airbyte_cdk.entrypoint import launch
+from source_icecat import SourceIcecat
+
+if __name__ == "__main__":
+    source = SourceIcecat()
+    launch(source, sys.argv[1:])
